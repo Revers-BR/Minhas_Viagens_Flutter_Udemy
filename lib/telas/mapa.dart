e972549b5056b702a5ea5e21f6aaa10bf3c909ed
Revers-BR <reversrivair@gmail.com>
2024-11-01 20:23:29 -0300

    Adicionado exibição de marcadores

--- lib/telas/mapa.dart
+++ lib/telas/mapa.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Mapa extends StatefulWidget {
@@ -14,6 +16,13 @@ class _Mapa extends State<Mapa> {
 
   final Set<Marker> marcadores = {};
 
+  CameraPosition _cameraPosition =  const CameraPosition(
+    target: LatLng(
+      -23.708762633339163, -46.62731574801392
+    ),
+    zoom: 18
+  );
+
   int contador = 0;
 
   final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
@@ -23,19 +32,61 @@ class _Mapa extends State<Mapa> {
     _controller.complete( controller );
   }
 
-  void _exibirMarcador(LatLng latLng) {
+  void _exibirMarcador(LatLng latLng) async {
 
-    final Marker marcador = Marker(
-      markerId: MarkerId((contador++).toString()),
-      position: latLng,
-      infoWindow: const InfoWindow(
-        title: "Marcador"
-      )
-    );
+    final latitude = latLng.latitude;
 
-    setState(() {
-      marcadores.add(marcador);
+    final longitude = latLng.longitude;
+
+    placemarkFromCoordinates(latitude, longitude).then((listaEndereco){
+      
+      if(listaEndereco.isNotEmpty){
+
+        Placemark endereco = listaEndereco[0];
+        String rua = endereco.thoroughfare!;
+
+        final Marker marcador = Marker(
+          markerId: MarkerId((contador++).toString()),
+          position: latLng,
+          infoWindow: InfoWindow(
+            title: rua
+          )
+        );
+
+        setState(() {
+          marcadores.add(marcador);
+        });
+      }
     });
+  }
+
+  _movimentarCamera() async {
+
+    final GoogleMapController googleMapController = await _controller.future;
+    googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+      _cameraPosition
+    ));
+  }
+
+  _addListenerLocalizacao(){
+
+    Geolocator.getPositionStream(
+      desiredAccuracy: LocationAccuracy.high
+    ).listen((position) {
+      
+        _cameraPosition = CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 18
+        );
+        _movimentarCamera();
+      });
+    
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _addListenerLocalizacao();
   }
 
   @override
@@ -47,14 +98,18 @@ class _Mapa extends State<Mapa> {
       ),
       body: GoogleMap(
         mapType: MapType.hybrid,
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(-23.708762633339163, -46.62731574801392),
-          zoom: 18
-        ),
+        initialCameraPosition: _cameraPosition,
         onMapCreated: _onMapCreated,
         onLongPress: _exibirMarcador,
+        myLocationEnabled: true,
         markers: marcadores,
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.future.then((controller) => controller.dispose());
   }
 }
